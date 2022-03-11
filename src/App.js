bdaeb17ac872d041388691a1d1a9fa75e154aedd
Danial-Waheed <daniyalwahied@gmail.com>
2022-03-11 22:24:52 +0500

    used react hooks useState and useRef

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
 
-function App() {
+export default function App() {
+  const START_TIME = 5;
+
+  const [text, setText] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(START_TIME);
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
+  const [wordCount, setWordCount] = useState(0);
+
+  const textBoxRef = useRef(null);
+
+  function handleChange(e) {
+    const { value } = e.target;
+    setText(value);
+  }
+
+  useEffect(() => {
+    if (isTimeRunning && timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining((time) => time - 1);
+      }, 1000);
+    } else if (timeRemaining === 0) {
+      endGame();
+    }
+  }, [timeRemaining, isTimeRunning]);
+
+  function calculateWordCount(text) {
+    const wordsArr = text.trim().split(" ");
+    return wordsArr.filter((word) => word !== "").length;
+  }
+  function startGame() {
+    setIsTimeRunning(true);
+    setTimeRemaining(START_TIME);
+    setText("");
+    textBoxRef.current.disabled = false;
+    textBoxRef.current.focus();
+  }
+  function endGame() {
+    setIsTimeRunning(false);
+    setWordCount(calculateWordCount(text));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Speed Typing Game</h1>
+      <textarea
+        onChange={handleChange}
+        disabled={!isTimeRunning}
+        value={text}
+        ref={textBoxRef}
+      />
+      <h4>Time Remaing: {timeRemaining}</h4>
+      <button onClick={startGame} disabled={isTimeRunning}>
+        Start Typing
+      </button>
+      <h1>Word Count: {wordCount}</h1>
     </div>
   );
 }
-
-export default App;
